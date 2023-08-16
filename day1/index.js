2ef54c92b8a41d1d5ecbc15851f378dcec72941b
snoopy1224 <misty12024@gmail.com>
2023-08-16 19:57:00 +0900

    기본 문법

---
+++ day1/index.js
@@ -0,0 +1,16 @@
+// function 함수(x :number) : number {
+//     return x * 2
+// }
+// // array에 쓸 수 있는 tuple 타입
+// type Member = [number, boolean];
+// let john: Member = [123, true];
+// type Member = {
+//   [key: string]: string;
+// };
+// let john: Member = { name: "kim" };
+// class User {
+//     name : string;
+//     constructor(name :string){
+//         this.name = name;
+//     }
+// }
